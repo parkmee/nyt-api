9183252d8c3ba564cdfe3ab9671dea7d53d73633
Mike <grossmi4@gmail.com>
2018-09-18 21:15:35 -0400

    wip

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,17 +6,31 @@ const apiKey = `45564cc502eb498f9245275f1fd5cc33`;
 //html Ids: term, records, start-year, end-year, search, clear
 //query parameters: q - search term; page - number of results; begin_date - start year; end_date - end year
 
-let search = $("#term").val().trim();
-let number = $("#records").val().trim();
-let startYear = $("#start-year").val().trim();
-let endYear = $("end-year").val().trim();
+let parameters = {
 
+  search: $("#term").val().trim();
+  number: $("#records").val().trim();
+  startYear: $("#start-year").val().trim();
+// let endYear = $("#end-year").val().trim();
+
+}
 $(document).ready(function(){
-  let queryURL = `${baseUrl}q=${$("#term").val().trim()}&apikey=${apiKey}`;
+
+  //set listener and AJAX return on search button click
+  $("#search").on("click",function(event) {
+    let queryURL = `${baseUrl}q=${search}&apikey=${apiKey}`;
     $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response)
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response)
     })
+  });
+
+  //set listener and clear function on clear button click
+  $("#clear").on("click",function(event){
+    $("#term").val('');
+    $("#records").val('');
+  })
+
 });
