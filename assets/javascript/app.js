7efaeb08bf6e81491a4e1d042f466de4547b5100
Mike <grossmi4@gmail.com>
2018-09-20 18:41:18 -0400

    ajax returns query based on parameters - no creation of divs yet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,32 +5,60 @@ const apiKey = `45564cc502eb498f9245275f1fd5cc33`;
 
 //html Ids: term, records, start-year, end-year, search, clear
 //query parameters: q - search term; page - number of results; begin_date - start year; end_date - end year
+let param = {
+  search: {
+    handle: "#term",
+    queryKey: 'q='
+  },
+  number: {
+    handle: "#records",
+    queryKey: 'page='
+  },
+  startYear: {
+    handle: "#start-year",
+    queryKey: 'begin_date='
+  },
+  endYear: {
+    handle: "#end-year",
+    queryKey: 'end_date'
+  }
+};
 
-let parameters = {
+const query = {
+  buildUrl: function(){
+    let queryTail = "";
+    let parametersList = [];
+    for (let par in param) {
+      parametersList.push(par);
+    }
+    console.log(parametersList);
+    for (let par of parametersList) {
+      let inputValue = $(param[par].handle).val().trim()
+      if (inputValue == "" ) {}
+      else{
+        queryTail += param[par].queryKey + $(param[par].handle).val().trim() + '&'
+      }
+    }
+    console.log(baseUrl + queryTail + 'apikey=' + apiKey);
+    return baseUrl + queryTail + 'apikey=' + apiKey;
+  },
+  emptyDivs: function(){
 
-  search: $("#term").val().trim();
-  number: $("#records").val().trim();
-  startYear: $("#start-year").val().trim();
-// let endYear = $("#end-year").val().trim();
+  }
+  }
+;
 
-}
-$(document).ready(function(){
-
-  //set listener and AJAX return on search button click
-  $("#search").on("click",function(event) {
-    let queryURL = `${baseUrl}q=${search}&apikey=${apiKey}`;
+$(document).ready(function() {
+  $("#search").on("click", function (event) {
+    event.preventDefault();
+    let queryURL = query.buildUrl();
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function (response) {
       console.log(response)
     })
-  });
-
-  //set listener and clear function on clear button click
-  $("#clear").on("click",function(event){
-    $("#term").val('');
-    $("#records").val('');
   })
+
 
 });
